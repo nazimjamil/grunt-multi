1fbfa059f997998f8daba3230c3df2b327110825
Naz <nazimjamil@gmail.com>
2015-07-28 19:49:28 +0100

    Grunt taskrunner with separated task files

---
+++ gruntfile.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+	require('load-grunt-tasks')(grunt, {
+		pattern: ['grunt-*']
+	});
+
+	require('time-grunt')(grunt);
+
+	grunt.initConfig({
+		
+		"pkg": grunt.file.readJSON('package.json'),
+
+		"settings": {}
+
+	});
+
+	grunt.loadTasks('tasks');
+
+};
---
+++ tasks/shell.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		
+		"shell": {
+			"test": {
+				"command": "echo 'hello'"
+			}
+		}
+		
+	});
+
+};
